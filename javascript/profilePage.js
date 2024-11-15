3cfc827cb4549006291df9a9043ce243468adfcb
Andrew Tran <andtran20@gmail.com>
2024-11-14 21:44:18 -0800

    Made a popup with javascript for comments

--- javascript/profilePage.js
+++ javascript/profilePage.js
@@ -1,3 +1,16 @@
-function createComment(){
-    
+const commentPopup = document.getElementById("commentPopup")
+
+const comment = document.getElementsByClassName("commentButton")
+commentButton.addEventListener("click",commentCreate) 
+
+const commentPopupBg = document.getElementById("commentPopupBackground")
+
+function commentCreate(){
+    commentPopup.classList.add("open")
+    commentPopupBg.classList.add("opaque")
+}
+
+function closePopup(){
+    commentPopup.classList.remove("open")
+    commentPopupBg.classList.remove("opaque")
 }

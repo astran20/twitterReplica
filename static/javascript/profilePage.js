5f14e0dd263f64675010c5fb293232cc024d5dcd
Andrew Tran <andtran20@gmail.com>
2024-11-21 02:35:41 -0800

    finished styling the popup / some js

--- static/javascript/profilePage.js
+++ static/javascript/profilePage.js
@@ -14,3 +14,18 @@ function closePopup(){
     commentPopup.classList.remove("open")
     commentPopupBg.classList.remove("opaque")
 }
+
+document.querySelectorAll('textarea').forEach( element => {
+    element.style.height = `${element.scrollHeight}px`;
+    element.addEventListener('input', event => {
+      event.target.style.height = 'auto'; 
+      event.target.style.height = `${event.target.scrollHeight}px`;
+    })
+  })
+
+function ready(){
+    postComment.style.opacity = "100%";
+    console.log("hello?")
+}
+
+

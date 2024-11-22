7f89e679afe1b6f56538bebd2ed51e38470b9811
Andrew Tran <andtran20@gmail.com>
2024-11-21 20:44:56 -0800

    stuff

--- static/javascript/profilePage.js
+++ static/javascript/profilePage.js
@@ -24,8 +24,36 @@ document.querySelectorAll('textarea').forEach( element => {
   })
 
 function ready(){
+    const postComment = document.getElementById("postComment")
     postComment.style.opacity = "100%";
-    console.log("hello?")
+    postComment.classList.add("ready")
 }
 
+function postComment(){
+  const createCommentTextData = document.getElementById('createCommentText').select()
+  const newCommentText = createCommonTextData.select()
+
+  if(postComment.classList.contains("ready")){
+    console.log("test")
+    const main = document.getElementsByTagName('main')
+    const newArticle = document.createElement("article")
+    main.appendChild(newArticle);
+  }
+  
+}
+
+const articles = document.querySelectorAll('article')
+const numPosts = articles.length
+
+updateNumPosts()
+function updateNumPosts(){
+  const numPostsIndicator = document.getElementById('numPostIndicator')
+  numPostsIndicator.textContent = numPosts;
+  const postAmountIndicator = document.getElementById('postAmount')
+  if(numPosts == 1){
+    postAmountIndicator.textContent = "post"
+  }else{
+    postAmountIndicator.textContent = "posts"
+  }
+}
 
